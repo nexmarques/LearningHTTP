f87b68ddc54f51a1fbce0052abeb11732ad84e2a
nexmarques <ale_ec_marques@hotmail.com>
2025-02-03 15:23:10 -0300

    Adding loading

--- App.js
+++ App.js
@@ -1,33 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from "react-native";
 import api from "./src/services/api";
 import Films from "./src/Films";
+
 
 export default function App() {
 
   const [films, setFilms] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadingFilms() {
       const response = await api.get('r-api/?api=filmes')
       setFilms(response.data);
+      setLoading(false)
     }
     loadingFilms();
   }, [])
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={films}
-        keyExtractor={ (item) => String(item.id) }
-        renderItem= { ({item}) => <Films data={item} />}
-      />
-    </View>
-  );
-}
+  if(loading){
+    return(
+      <View style={styles.loading} >
+        <ActivityIndicator
+          size={50}
+          color="#121212"
+        />
+      </View>
+    )
+  }      
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={films}
+          keyExtractor={ (item) => String(item.id) }
+          renderItem= { ({item}) => <Films data={item} />}
+        />
+      </View>
+    );
+  }
+  
+    
+   
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,    
   },
+  loading: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
